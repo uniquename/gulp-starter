7d67a008c3baadf2e1d2d6779d264c390f701d4b
Hendrik Kuck <h@12deg.de>
2017-07-12 09:10:08 +0200

    added bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var sass        = require('gulp-sass');
 var watch       = require('gulp-watch');
 var connect     = require('gulp-connect');
 var opn         = require('opn');
-let cleanCSS    = require('gulp-clean-css');
-let sourcemaps  = require('gulp-sourcemaps');
+var cleanCSS    = require('gulp-clean-css');
+var sourcemaps  = require('gulp-sourcemaps');
 var rename      = require("gulp-rename");
 var uglify      = require('gulp-uglify');
 var concat      = require('gulp-concat');
@@ -26,6 +26,10 @@ var server = {
   port: '8080'
 }
 
+var bootstrap = {
+  js: ['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'],
+  sass: ['node_modules/bootstrap/scss/bootstrap.scss']
+}
 
 gulp.task('watch', function() {
   gulp.watch('src/styles/**/*.scss',['styles']);
@@ -63,6 +67,25 @@ gulp.task('js', function () {
     .pipe(connect.reload());
 });
 
+gulp.task('bs_styles', function() {
+  gulp.src(bootstrap.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(destPaths.styles))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(destPaths.styles))
+    .pipe(connect.reload());
+});
+
+// Move the javascript files into our /src/js folder
+gulp.task('bs_js', function() {
+  gulp.src(bootstrap.js)
+    .pipe(gulp.dest(destPaths.js))
+    .pipe(connect.reload());
+});
+
 gulp.task('openbrowser', function() {
   opn( 'http://' + server.host + ':' + server.port );
 });
@@ -76,6 +99,6 @@ gulp.task('webserver', ['watch'], function () {
   });
 });
 
-gulp.task('build', ['styles', 'html', 'js']);
+gulp.task('build', ['styles', 'html', 'js', 'bs_styles', 'bs_js']);
 
 gulp.task('default', ['build', 'webserver', 'watch', 'openbrowser']);
